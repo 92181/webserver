3277eaa50f00b25784a0bbfeb823ae961dfba1a3
Wolf Schulz <82281132+92181@users.noreply.github.com>
2025-03-01 23:20:48 +0100

    Improved memory safety

--- examples/static/main.c
+++ examples/static/main.c
@@ -8,37 +8,100 @@
 
 // gcc main.c -o main -O3 -lssl -lcrypto, https://[::1]:3000/
 
-unsigned char *b,*c,*qw=(unsigned char*)"\033[31mMemory Allocation Failure!\033[0m\n",*qe=(unsigned char*)"\033[31mFile Operation Failed!\n\033[0m",*u,*t,*j,**gh,**sf,**se;struct stat d;unsigned int gl,gj=0;
+unsigned char *b,*c,qw[]="\033[31mMemory Allocation Failure!\033[0m\n",qe[]="\033[31mFile Operation Failed!\033[0m\n",*u,*t,*j,**gh,**sf,**se;
 
-unsigned int i[4],s,a,f;char vq[16384],*vz,*vw,*vy,*gi="index.html",*gu;
+char vq[16384],*vz,*vw,*vy,*gi="index.html",*gu;
+unsigned int gl,gj=0,i[4],s,a,f;
+
+struct stat d;
 
 // String Length Function;
-static inline unsigned long int srl(char *p){char *i=p;while(*p!=0){p+=1;};return p-i;};
+static inline unsigned long int srl(char *p)
+{
+  char *i=p;
+  
+  while(*p!=0)
+  {
+    p+=1;
+  };
+  
+  return p-i;
+};
 
 // Hash Bytes Using FNV Hashing Algorithm;
 static inline unsigned int fnv(char *i,char *u)
 {
   // Hash In Groups Of 4 Bytes;
-  unsigned int h=2166136261;while(i+4<u){h=(h^*i)*16777619;h=(h^*(i+1))*16777619;h=(h^*(i+2))*16777619;h=(h^*(i+3))*16777619;i+=4;};
+  unsigned int h=2166136261;
+  
+  while(i+4<u)
+  {
+    h=(h^*i)*16777619;
+    h=(h^*(i+1))*16777619;
+    h=(h^*(i+2))*16777619;
+    h=(h^*(i+3))*16777619;
+    
+    i+=4;
+  };
   
   // Deal With Remaining Bytes And Return;
-  while(i<u){h=(h^*i)*16777619;i+=1;};return h;
+  while(i<u)
+  {
+    h=(h^*i)*16777619;i+=1;
+  };
+  
+  return h;
 };
 
 // Load All Files In Given Directory And Create Hashmap;
 static inline void trd(char *g,unsigned int b)
 {
-  // Allocate Variables (May Overflow);
-  char st[131072],*y=st,*pt[2048],**jx=pt,cd[8192];t=u=malloc(1024*1024);j=u+1024*1024;
+  // Allocate Buffer Memory (May Overflow);
+  char st[131072],*y=st,*pt[2048],**jx=pt,*cd=malloc(1024),**ui=pt+sizeof(pt),*op=st+sizeof(st),*yp=cd+sizeof(cd);
   
-  // Write Name To Stack;
-  *jx=y;while(*g!=0){*y=*g;y+=1;g+=1;};*y=0;y+=1;
+  t=u=malloc(1024*1024);j=u+1024*1024;
 
+  // Write Entry Point To Stack;
+  char *gt=y+b;
+
+  if(gt>op)
+  {
+    return;
+  };
+
+  *jx=y;
+
+  while(y<gt)
+  {
+    *y=*g;y+=1;g+=1;
+  };
+  
   // Loop Through Saved Directories;
   while(jx>=pt)
   {
-    // Write Current Directory To Seperate Buffer;
-    g=cd;y=*jx;while(*y!=0){*g=*y;y+=1;g+=1;};*g=0;y=*jx;jx-=1;
+    // Write Current Directory To Active Buffer;
+    g=cd;y=*jx;
+
+    char *py=cd+srl(y);
+    
+    if(py>yp)
+    {
+      unsigned int nws=(yp-cd)+2048;cd=g=realloc(cd,nws);
+
+      if(cd==0)
+      {
+        return;
+      };
+
+      yp=cd+nws;
+    };
+    
+    while(g<py)
+    {
+      *g=*y;y+=1;g+=1;
+    };
+
+    *g=0;y=*jx;jx-=1;
 
     // Open Directory And Make Ready For Path Fill;
     struct dirent *en;DIR *dr=opendir((const char*)cd);if(dr==0){continue;};*g='/';g+=1;
@@ -46,16 +109,36 @@ static inline void trd(char *g,unsigned int b)
     while((en=readdir(dr))!=0)
     {
       // Get Name And Skip Hidden Files;
-      char *v=en->d_name,*tu=g,*ty=tu+srl(v),*lr;if(*v=='.'){continue;};
+      char *v=en->d_name,*tu=g,*ty=tu+srl(v),*lr;
+      
+      if(*v=='.')
+      {
+        continue;
+      };
 
       // Write Extension Of Path;
       while(tu<ty){*tu=*v;v+=1;tu+=1;};*tu=0;
+
+      char *up=y+(ty-cd);
       
       // Save Directories To Stack, Else Load Files Into Memory;
       if(en->d_type==4)
       {
-        // Save To Stack (May Overflow);
-        lr=cd;jx+=1;*jx=y;while(*lr!=0){*y=*lr;y+=1;lr+=1;};*y=0;y+=1;
+        // Prevent Memory Overflow;
+        if(jx+1>ui||up>op)
+        {
+          write(1,qw,sizeof(qw));exit(1);
+        };
+
+        // Save To Stack;
+        lr=cd;jx+=1;*jx=y;
+        
+        while(y<up)
+        {
+          *y=*lr;y+=1;lr+=1;
+        };
+        
+        *y=0;y+=1;
       }
       else
       {
@@ -121,11 +204,11 @@ static inline void router(SSL *q)
 int main(int e,char **r)
 {
   // Load Files Into Memory And Create References;
-  trd("./public",8+1);
+  char t[]="./public";trd(t,sizeof(t));
 
   // Load Certificates;
-  f=open("cert.pem",0);if(f>>31){write(1,qe,33);return 1;};fstat(f,&d);s=d.st_size;b=malloc(s);read(f,b,s);close(f);
-  f=open("key.pem",0);if(f>>31){write(1,qe,33);return 1;};fstat(f,&d);a=d.st_size;c=malloc(a);read(f,c,a);close(f);
+  f=open("cert.pem",0);if(f>>31){write(1,qe,sizeof(qe));return 1;};fstat(f,&d);s=d.st_size;b=malloc(s);read(f,b,s);close(f);
+  f=open("key.pem",0);if(f>>31){write(1,qe,sizeof(qe));return 1;};fstat(f,&d);a=d.st_size;c=malloc(a);read(f,c,a);close(f);
 
   // Start Server On Port 3000;
   *(i+3)=16777216;insrv(i,3000,router,b,s,c,a);free(b);free(c);pthread_join(*nu,0);
